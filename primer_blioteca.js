3e69fd3568bc532ba7bb8ce04fa85deae8bf95dd
markevets <131561438+markevets@users.noreply.github.com>
2023-06-07 21:19:03 -0500

    Add files via upload

---
+++ primer_blioteca.js
@@ -0,0 +1,30 @@
+var fraction=function(x,n,d){
+    return x/d*n;
+};
+var sumar=function(num1,num2){
+    return num1+num2;
+};
+var restar =function(num1,num2){
+    return num1-num2;
+};
+var multiplicar =function(num1,num2){
+    return num1*num2;
+};
+var dividir=function(num1,num2){
+    return num1/num2; 
+};
+/*var potenciacion=function(num1,num2){
+    return num1^num2;
+};*/
+
+var triangulo=function(alto,ancho){
+    return (alto*ancho)/2;};
+var cuadrado=function(mlado) {
+    return mlado*mlado;
+};
+var rectangulo=function(alto,ancho){
+    return alto*ancho;
+};
+var fdc=function(d,n,c){
+    return c/d*n;
+};
